c08d48e815bec4885b3af51642cd8baba91364a4
MINH HOANG NGUYEN <s223669184@ai.deakin.edu.au>
2024-05-02 12:13:18 +1000

    Initial commit with code part B

---
+++ resources/code/partB/utils.py
@@ -0,0 +1,44 @@
+import os
+
+import numpy as np
+import gym
+import torch 
+
+import torch.nn as nn
+
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+    
+
+class MinigridFeaturesExtractor(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.Space, features_dim: int = 512, normalized_image: bool = False) -> None:
+        super().__init__(observation_space, features_dim)
+        n_input_channels = observation_space.shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 16, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, (2, 2)),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with torch.no_grad():
+            n_flatten = self.cnn(torch.as_tensor(observation_space.sample()[None]).float()).shape[1]
+
+        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        return self.linear(self.cnn(observations))
+
+def get_policy_kwargs():
+    policy_kwargs = dict(
+        features_extractor_class=MinigridFeaturesExtractor,
+        features_extractor_kwargs=dict(features_dim=128),
+    )
+    return policy_kwargs
+
+
+
+
